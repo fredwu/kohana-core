b2da75497f48065be1a19844904d7cdbf1b21862
Fred Wu <canglan@gmail.com>
2010-07-01 00:15:15 +1000

    Added three parameters to Kohana_Log_File::write():
    * $prepend: set a string to prepend it to the log file, `TRUE` to prepend the default Kohana security line, `FALSE` to disable it.
    * $sub_directories: `FALSE` to disable the `YYYY/MM/` directory structure.
    * $custom_filename: set a string to use it as the filename, `FALSE` to use the current day as the filename.
    
    This offers more flexibility for the app and the modules to create custom log files.

--- classes/kohana/log/file.php
+++ classes/kohana/log/file.php
@@ -35,48 +35,33 @@ class Kohana_Log_File extends Kohana_Log_Writer {
 	}
 
 	/**
-	 * Writes each of the messages into the log file. The log file will be
-	 * appended to the `YYYY/MM/DD.log.php` file, where YYYY is the current
-	 * year, MM is the current month, and DD is the current day.
+	 * Writes each of the messages into the log file. By default the log file
+	 * will be appended to the `YYYY/MM/DD.log.php` file, where YYYY is the
+	 * current year, MM is the current month, and DD is the current day.
 	 *
 	 *     $writer->write($messages);
 	 *
-	 * @param   array   messages
+	 * @param   array    messages
+	 * @param   mixed    prepend          set a string to prepend it to the log file,
+	 *                                    `TRUE` to prepend the default Kohana security line,
+	 *                                    `FALSE` to disable it
+	 * @param   boolean  sub_directories  `FALSE` to disable the `YYYY/MM/` directory structure
+	 * @param   mixed    custom_filename  set a string to use it as the filename,
+	 *                                    `FALSE` to use the current day as the filename
 	 * @return  void
 	 */
-	public function write(array $messages)
+	public function write(array $messages, $prepend = TRUE, $sub_directories = TRUE, $custom_filename = FALSE)
 	{
-		// Set the yearly directory name
-		$directory = $this->_directory.date('Y').DIRECTORY_SEPARATOR;
-
-		if ( ! is_dir($directory))
-		{
-			// Create the yearly directory
-			mkdir($directory, 0777);
-
-			// Set permissions (must be manually set to fix umask issues)
-			chmod($directory, 0777);
-		}
-
-		// Add the month to the directory
-		$directory .= date('m').DIRECTORY_SEPARATOR;
-
-		if ( ! is_dir($directory))
-		{
-			// Create the yearly directory
-			mkdir($directory, 0777);
-
-			// Set permissions (must be manually set to fix umask issues)
-			chmod($directory, 0777);
-		}
-
+		$directory = $this->which_directory($sub_directories);
+		
 		// Set the name of the log file
-		$filename = $directory.date('d').EXT;
+		$filename = is_string($custom_filename) ? $custom_filename : date('d').EXT;
+		$filename = $directory.$filename;
 
 		if ( ! file_exists($filename))
 		{
 			// Create the log file
-			file_put_contents($filename, Kohana::FILE_SECURITY.' ?>'.PHP_EOL);
+			file_put_contents($filename, $this->prepend_content($prepend));
 
 			// Allow anyone to write to log files
 			chmod($filename, 0666);
@@ -90,6 +75,74 @@ class Kohana_Log_File extends Kohana_Log_Writer {
 			// Write each message into the log file
 			file_put_contents($filename, PHP_EOL.strtr($format, $message), FILE_APPEND);
 		}
+	}
+	
+	/**
+	 * Determines the log file directory structure
+	 *
+	 * @param   boolean  sub_directories  `FALSE` to disable the `YYYY/MM/` directory structure
+	 * @return  string                    the directory
+	 */
+	private function which_directory($sub_directories = TRUE)
+	{
+		if ($sub_directories === TRUE)
+		{
+			// Set the yearly directory name
+			$directory = $this->_directory.date('Y').DIRECTORY_SEPARATOR;
+
+			if ( ! is_dir($directory))
+			{
+				// Create the yearly directory
+				mkdir($directory, 0777);
+
+				// Set permissions (must be manually set to fix umask issues)
+				chmod($directory, 0777);
+			}
+
+			// Add the month to the directory
+			$directory .= date('m').DIRECTORY_SEPARATOR;
+
+			if ( ! is_dir($directory))
+			{
+				// Create the yearly directory
+				mkdir($directory, 0777);
+
+				// Set permissions (must be manually set to fix umask issues)
+				chmod($directory, 0777);
+			}
+		}
+		else
+		{
+			$directory = $this->_directory;
+		}
+		
+		return $directory;
+	}
+	
+	/**
+	 * Content to prepend to the log file
+	 *
+	 * @param   mixed    prepend          set a string to prepend it to the log file,
+	 *                                    `TRUE` to prepend the default Kohana security line,
+	 *                                    `FALSE` to disable it
+	 * @return  string                    the content to be prepended
+	 */
+	private function prepend_content($prepend = TRUE)
+	{
+		if ($prepend === TRUE)
+		{
+			$prepend_content = Kohana::FILE_SECURITY.' ?>'.PHP_EOL;
+		}
+		elseif (is_string($prepend))
+		{
+			$prepend_content = $prepend.PHP_EOL;
+		}
+		else
+		{
+			$prepend_content = NULL;
+		}
+		
+		return $prepend_content;
 	}
 
 } // End Kohana_Log_File
